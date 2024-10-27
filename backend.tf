ceca6b81b226a6e61939b267e8da39295a88ceff
tumidola <145950223+tumidola@users.noreply.github.com>
2024-10-27 08:11:14 +0100

    Update backend.tf
    updating region

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-devterraform-s3-bucket"
     key = "main"
-    region = "us-east-1"
+    region = "us-west-2"
     dynamodb_table = "my-devterraform-ddb-table"
   }
 }
